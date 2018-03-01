ea9fa0b5aab46ce1f9ef43050031d74acc49a0da
dkomnen <davidkomljenovic93@gmail.com>
2018-03-01 22:09:48 +0100

    hacked together some stuff so it just works (in need of refactor)

--- cmd/valkyrie/main.go
+++ cmd/valkyrie/main.go
@@ -3,8 +3,14 @@ package main
 import (
 	"log"
 	"os"
+	"os/signal"
+	"syscall"
 	"time"
 
+	"github.com/dkomnen/iot-bridge/broker"
+	"github.com/dkomnen/iot-bridge/cmd/valkyrie/device"
+	"github.com/dkomnen/iot-bridge/cmd/valkyrie/device/temp"
+	"github.com/dkomnen/iot-bridge/mqtt"
 	"github.com/urfave/cli"
 )
 
@@ -16,14 +22,6 @@ func main() {
 	app.Usage = "mock an IOT device for testing Bifrost"
 	app.Version = "0.0.0"
 	app.Author = "David Komljenovic"
-
-	app.Flags = []cli.Flag{
-		cli.StringFlag{
-			Name:  "broker-address",
-			Usage: "set the address for the broker",
-			Value: "tcp://localhost:1883",
-		},
-	}
 
 	app.Commands = cli.Commands{
 		cli.Command{
@@ -57,9 +55,55 @@ func main() {
 					Name:  "fahrenheit, f",
 					Usage: "if set, the unit of measurement will be fahrenheit",
 				},
+				cli.StringFlag{
+					Name:  "broker-address",
+					Usage: "set the address for the broker",
+					Value: "tcp://localhost:1883",
+				},
+				cli.StringFlag{
+					Name:  "serial-number, s",
+					Usage: "string which will be hashed and used as serial number of the device",
+					Value: "temp",
+				},
 			},
 			Action: func(ctx *cli.Context) error {
-				// TODO: run the temp device type
+				baddr := ctx.String("broker-address")
+				log.Println("broker address:", baddr)
+				b := mqtt.New(
+					broker.Address(baddr),
+					mqtt.ClientID("temp"),
+				)
+
+				t := temp.New(
+					device.Broker(b),
+					device.SerialNumber(
+						device.GenerateSerialNumber(ctx.String("serial-number")),
+					),
+					device.Interval(ctx.Duration("tick")),
+					temp.DataRange(
+						ctx.Float64("lower-limit"),
+						ctx.Float64("upper-limit"),
+					),
+					temp.Fahrenheit(ctx.Bool("fahrenheit")),
+				)
+
+				stop := make(chan os.Signal, 1)
+				signal.Notify(stop, syscall.SIGINT, syscall.SIGTERM)
+
+				if err := t.Setup(); err != nil {
+					return err
+				}
+				go func() {
+					<-stop
+					if err := t.Stop(); err != nil {
+						log.Fatal(err)
+					}
+				}()
+
+				if err := t.Run(); err != nil {
+					return err
+				}
+
 				return nil
 			},
 		},
